3ff06f637f526bb224976a913115535a8c644c1e
YonatanAltman <yonatan@yaltman.com>
2019-10-23 19:13:09 +0300

    3 files modified

--- src/app/catalog/hey/hey.component.ts
+++ src/app/catalog/hey/hey.component.ts
@@ -10,6 +10,7 @@ import { map } from "rxjs/operators";
 })
 export class HeyComponent implements OnInit, OnDestroy {
   public count: number;
+  public count1: number;
   private subscription = new Subscription();
   constructor(private app: AppService) {
     const sub = this.app.count$
@@ -20,7 +21,16 @@ export class HeyComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe();
+    const sub1 = this.app.count$
+      .pipe(
+        map(num => {
+          console.log("works" + num);
+          this.count1 = (num as number) * 13;
+        })
+      )
+      .subscribe();
     this.subscription.add(sub);
+    this.subscription.add(sub1);
   }
 
   ngOnInit() {}
